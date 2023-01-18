81bd365484361ea3a89b8c9f025c3cc2a6d3f1b4
femi <oluwafemi.akinyemi@datamellon.com>
2023-01-18 12:22:19 +0100

    progress

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -10,55 +10,47 @@
       <a href="#/" class="md:hidden absolute top-1 right-14">
         <div class="relative">
           <div class="relative">
-            <Icon src={AiOutlineShoppingCart} />
+            <Icon src="{AiOutlineShoppingCart}" />
             {#if productId?.length >= 1}
-              <div
-                class="absolute px-1  bg-red-400 -top-1 -right-1 rounded-full border-2 border-white text-white"
-                id="cart"
-                style="font-size: 10px"
-              >
-                {productId?.length}
-              </div>
+            <div
+              class="absolute px-1 bg-red-400 -top-1 -right-1 rounded-full border-2 border-white text-white"
+              id="cart"
+              style="font-size: 10px"
+            >
+              {productId?.length}
+            </div>
             {/if}
           </div>
         </div>
       </a>
 
-      <div
-        class="flex-grow font-bold text-lg"
-
-      >
+      <div class="flex-grow font-bold text-lg">
         <a href="/">
           <span>Best Store</span>
         </a>
       </div>
+
       <div class="menu hidden md:flex flex-col md:flex-row mt-5 md:mt-0 gap-16">
         <div class="flex flex-col md:flex-row gap-12 capitalize">
-          <div
-            class="text-red-400 font-bold border-b border-red-400"
-
-          >
+          <div class="text-red-400 font-bold border-b border-red-400">
             <a href="/"> home</a>
           </div>
-          <div
-            class="text-red-400 font-bold border-b border-red-400"
-
-          >
+          <div class="text-red-400 font-bold border-b border-red-400">
             <a href="/products">products</a>
           </div>
         </div>
         <div class="flex gap-12">
           <a href="#/" class="hidden md:block">
             <div class="relative">
-              <Icon src={AiOutlineShoppingCart} />
+              <Icon src="{AiOutlineShoppingCart}" />
               {#if productId?.length >= 1}
-                <div
-                  class="absolute px-1  bg-red-400 -top-1 -right-1 rounded-full border-2 border-white text-white"
-                  id="cart"
-                  style="font-size: 10px"
-                >
-                  {productId?.length}
-                </div>
+              <div
+                class="absolute px-1 bg-red-400 -top-1 -right-1 rounded-full border-2 border-white text-white"
+                id="cart"
+                style="font-size: 10px"
+              >
+                {productId?.length}
+              </div>
               {/if}
             </div>
           </a>
